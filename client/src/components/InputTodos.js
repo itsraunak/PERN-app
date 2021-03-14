a49af5f1832b3c14e47be5cbf88bffc3756fe7df
Raunak <itisraunak@gmail.com>
2021-03-14 20:46:31 +0530

    added react.js

--- client/src/components/InputTodos.js
+++ client/src/components/InputTodos.js
@@ -9,7 +9,7 @@ const InputTodo = () =>
             <h1 className="text-center mt-5">Pern Todo List</h1>
             <form className="d-flex mt-5">
                 <input type="text" className="form-control" value={description} onChange={e => 
-                setDescription()} />
+                setDescription(e.target)} />
                 <button className ="btn btn-success">Add</button>
             </form>
         </Fragment>
